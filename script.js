012e1a7f97cdd734cd46e607306053ff61af32f4
SmoothStar <adityakute24@gmail.com>
2024-09-21 11:07:06 +0530

    updating the project

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//this is imp for taking the user if logged in
 // window.addEventListener('load',(ev) => {
 //     const isloggedIn =localStorage.getItem("loggedInuser");
 
@@ -10,9 +9,38 @@
 //         window.location="/"
 //     }
 // })
-function signin(){
-    window.location="dashboard.html"
-}
+// function signin(){
+//     window.location="login.html"
+// }
 function questionPage(){
     window.location="question.html"
+}
+function signup(){
+    const email =document.getElementById("email").value;
+    const password =document.getElementById("password").value;
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    
+
+    if(!email){
+        alert("Please enter your email")
+        return;
+    }
+    if (!emailPattern.test(email)) {
+        alert("not correct email")
+        return;
+    }
+
+    
+
+    if (!password) {
+        alert("Please Enter your password")
+        return;
+    }
+    if (password.length<8) {
+        alert("Please Enter Atleast 8 characters")
+        return;
+    }
+
+     window.location="login.html"
 }
